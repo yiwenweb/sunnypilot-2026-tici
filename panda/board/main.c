61af3611f404e19148646d71f2c5a84d9a11cc98
yiwen <yiwen@sunnypilot.com>
2026-09-01 15:29:59 +0800

    fix: guard H7-only dts_init() behind STM32H7 for F4 build

--- panda/board/main.c
+++ panda/board/main.c
@@ -289,7 +289,9 @@ int main(void) {
   led_set(LED_RED, true);
   led_set(LED_GREEN, true);
   adc_init(ADC1);
+#ifdef STM32H7
   dts_init();
+#endif
 
   // print hello
   print("\n\n\n************************ MAIN START ************************\n");
