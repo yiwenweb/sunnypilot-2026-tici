b1e4e25de2a5dd2514038980b05c1d0c9913d1da
yiwen <yiwen@sunnypilot.com>
2026-09-01 15:24:31 +0800

    fix: merge controls_allowed lateral/longitudinal into sp_pkt in main_comms.h

--- panda/board/main_comms.h
+++ panda/board/main_comms.h
@@ -57,8 +57,7 @@ static int get_health_pkt(void *dat) {
   health->sound_output_level_pkt = sound_output_level;
   #endif
 
-  health->controls_allowed_lateral_pkt = controls_allowed || controls_allowed_lateral;
-  health->controls_allowed_longitudinal_pkt = controls_allowed;
+  health->controls_allowed_sp_pkt = (controls_allowed || controls_allowed_lateral) | (controls_allowed << 1);
 
   return sizeof(*health);
 }
